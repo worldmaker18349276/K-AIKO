ef4aaf9a9eb0a1c9f8795ce55a6fe77efed7b555
worldmaker <worldmaker18349276@gmail.com>
2020-07-19 07:52:53 +0800

    improve realtime_spectrogram

--- realtime_spectrogram.py
+++ realtime_spectrogram.py
@@ -1,5 +1,6 @@
 import librosa
 import numpy
+import scipy.signal
 import pyaudio
 import time
 
@@ -9,13 +10,15 @@ RATE = 44100
 FRAMES_PER_BUFFER = 1024
 
 SCREEN_WIDTH = 178
-ENERGY_THRESHOLD = 0.1
+ENERGY_THRESHOLD = 0.02
 
 # Choose the frequency range of your log-spectrogram.
 N_FFT = 4096
 F_LO = librosa.note_to_hz('C2')
 F_HI = librosa.note_to_hz('C9')
-M = librosa.filters.mel(RATE, N_FFT, SCREEN_WIDTH, fmin=F_LO, fmax=F_HI) / ENERGY_THRESHOLD
+bank = librosa.filters.mel(RATE, N_FFT, SCREEN_WIDTH, fmin=F_LO, fmax=F_HI)
+bank *= scipy.signal.get_window('hann', N_FFT//2+1)[None,:]
+bank /= ENERGY_THRESHOLD
 
 def melspectrum_from_audio(audio_data):
     # This function takes one audio buffer as a numpy array and returns a string to be printed to the terminal.
@@ -23,7 +26,7 @@ def melspectrum_from_audio(audio_data):
     x_fft = numpy.fft.rfft(audio_data, n=N_FFT)
 
     # Compute mel spectrum.
-    melspectrum = M.dot(abs(x_fft)).astype(int)
+    melspectrum = bank.dot(abs(x_fft)).astype(int)
 
     return melspectrum
 
@@ -51,9 +54,9 @@ def show_spectrogram(melspectrum):
     return ''.join(char_list)
 
 def callback(in_data, frame_count, time_info, status):
-    audio_data = numpy.fromstring(in_data, dtype=numpy.float32)
+    audio_data = numpy.frombuffer(in_data, dtype=numpy.float32)
     spectrum = melspectrum_from_audio(audio_data)
-    output_str = show_spectrum(spectrum)
+    output_str = show_spectrogram(spectrum)
     print(output_str)
     return in_data, pyaudio.paContinue
 
