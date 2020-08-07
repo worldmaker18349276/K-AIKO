8cb96eb89aaa7be11ddeb39e85fcdee7643e844f
worldmaker <worldmaker18349276@gmail.com>
2020-08-07 09:08:52 +0800

    rewrite enum by class, add key judgement

--- game.py
+++ game.py
@@ -1,7 +1,5 @@
 import sys
 import time
-import collections
-import enum
 import numpy as np
 import pyaudio
 import realtime_analysis as ra
@@ -23,42 +21,45 @@ DELTA = 0.1
 DISPLAY_FPS = 200
 DISPLAY_DELAY = -0.03
 
-def sign(value):
-    if value > 0:
-        return 1
-    elif value < 0:
-        return -1
-    else:
-        return 0
-
 
 # beats and hits
-class BeatType(enum.Enum):
-    SOFT = 1
-    LOUD = 2
-    # ROLL = 3
-    # FLAM = 4
+class Beat:
+    def __init__(self, time):
+        self.time = time
 
-def Soft(time):
-    return Beat(time, BeatType.SOFT)
+    def __repr__(self):
+        return "Beat.{}(time={})".format(type(self).__name__, repr(self.time))
 
-def Loud(time):
-    return Beat(time, BeatType.LOUD)
-
-Beat = collections.namedtuple("Beat", ["time", "type"])
+Beat.Soft = type("Soft", (Beat,), dict())
+Beat.Loud = type("Loud", (Beat,), dict())
 
 
-class HitScore(enum.Enum):
-    GREATE     = (3, 0)
-    GOOD_LATE  = (2, 1)
-    GOOD_EARLY = (2, -1)
-    BAD_LATE   = (1, 1)
-    BAD_EARLY  = (1, -1)
-    MISS_LATE  = (0, 1)
-    MISS_EARLY = (0, -1)
-    MISS       = (0, 0)
+class Hit:
+    def __init__(self, time, strength):
+        self.time = time
+        self.strength = strength
 
-Hit = collections.namedtuple("Hit", ["time", "strength", "score", "index"])
+    def __repr__(self):
+        return "Hit.{}(time={}, strength={})".format(type(self).__name__, repr(self.time), repr(self.strength))
+
+Hit.Miss       = type("Miss",       (Hit,), dict(score=0, accuracy=0))
+Hit.Unexpected = type("Unexpected", (Hit,), dict(score=0, accuracy=0))
+
+Hit.Great       = type("Great",       (Hit,), dict(score=10, accuracy=3))
+Hit.LateGood    = type("LateGood",    (Hit,), dict(score=5, accuracy=2))
+Hit.EarlyGood   = type("EarlyGood",   (Hit,), dict(score=5, accuracy=2))
+Hit.LateBad     = type("LateBad",     (Hit,), dict(score=3, accuracy=1))
+Hit.EarlyBad    = type("EarlyBad",    (Hit,), dict(score=3, accuracy=1))
+Hit.LateFailed  = type("LateFailed",  (Hit,), dict(score=0, accuracy=0))
+Hit.EarlyFailed = type("EarlyFailed", (Hit,), dict(score=0, accuracy=0))
+
+Hit.WrongButGreat       = type("WrongButGreat",       (Hit,), dict(score=5, accuracy=3))
+Hit.WrongButLateGood    = type("WrongButLateGood",    (Hit,), dict(score=3, accuracy=2))
+Hit.WrongButEarlyGood   = type("WrongButEarlyGood",   (Hit,), dict(score=3, accuracy=2))
+Hit.WrongButLateBad     = type("WrongButLateBad",     (Hit,), dict(score=1, accuracy=1))
+Hit.WrongButEarlyBad    = type("WrongButEarlyBad",    (Hit,), dict(score=1, accuracy=1))
+Hit.WrongButLateFailed  = type("WrongButLateFailed",  (Hit,), dict(score=0, accuracy=0))
+Hit.WrongButEarlyFailed = type("WrongButEarlyFailed", (Hit,), dict(score=0, accuracy=0))
 
 
 class Beatmap:
@@ -71,79 +72,123 @@ class Beatmap:
         return ra.clicks(times, sr=RATE, duration=self.duration)
 
 
-def judge(beatmap, scores, thresholds, judgements):
-    beat_iter = enumerate(beatmap.beats)
-    index, beat = next(beat_iter, (None, None))
+def judge(beats, hits, thresholds, judgements):
+    beats = iter(beats)
+    beat = next(beats, None)
 
     while True:
         time, strength, detected = yield
 
-        while index is not None and beat.time + judgements[0] <= time:
-            scores.append(Hit(time, 0, HitScore.MISS, index))
-            index, beat = next(beat_iter, (None, None))
+        # if next beat has passed through
+        while beat is not None and beat.time + judgements[0] <= time:
+            hits.append(Hit.Miss(time, 0))
+            beat = next(beats, None)
 
-        if detected:
-            if index is not None and beat.time - judgements[0] <= time:
-                err = time - beat.time
-                loudness = next((i+1 for i, thr in reversed(list(enumerate(thresholds))) if strength >= thr), 0)
-                accuracy = next((i for i, acc in reversed(list(enumerate(judgements))) if abs(err) < acc), 0)
-                sgn = sign(err) if accuracy != len(judgements)-1 else 0
+        if not detected:
+            continue
 
-                scores.append(Hit(time, strength, HitScore((accuracy, sgn)), index))
+        # if next beat isn't in the range yet
+        if beat is None or beat.time - judgements[0] > time:
+            hits.append(Hit.Unexpected(time, strength))
+            continue
 
-                index, beat = next(beat_iter, (None, None))
+        # judge pressed key (determined by loudness)
+        loudness = next((i+1 for i, thr in reversed(list(enumerate(thresholds))) if strength >= thr), 0)
 
+        if isinstance(beat, Beat.Soft):
+            is_correct_key = loudness == 1
+        elif isinstance(beat, Beat.Loud):
+            is_correct_key = loudness == 2
+
+        # judge accuracy
+        err = time - beat.time
+        accuracy = next((i for i, acc in reversed(list(enumerate(judgements))) if abs(err) < acc), 0)
+
+        if accuracy == 0:
+            if is_correct_key:
+                hit_type = Hit.LateFailed         if err > 0 else Hit.EarlyFailed
             else:
-                scores.append(Hit(time, strength, HitScore.MISS, None))
+                hit_type = Hit.WrongButLateFailed if err > 0 else Hit.WrongButEarlyFailed
 
-def draw_track(beatmap, scores, thresholds, sustain, track_width, bar_offset, drop_speed):
+        elif accuracy == 1:
+            if is_correct_key:
+                hit_type = Hit.LateBad            if err > 0 else Hit.EarlyBad
+            else:
+                hit_type = Hit.WrongButLateBad    if err > 0 else Hit.WrongButEarlyBad
+
+        elif accuracy == 2:
+            if is_correct_key:
+                hit_type = Hit.LateGood           if err > 0 else Hit.EarlyGood
+            else:
+                hit_type = Hit.WrongButLateGood   if err > 0 else Hit.WrongButEarlyGood
+
+        elif accuracy == 3:
+            if is_correct_key:
+                hit_type = Hit.Great
+            else:
+                hit_type = Hit.WrongButGreat
+
+        hits.append(hit_type(time, strength))
+        beat = next(beats, None)
+
+
+def draw_track(beats, hits, track_width, bar_offset, drop_speed, thresholds, sustain):
     dt = 1 / drop_speed
 
-    beats_iter = ((i, b, int(b.time/dt)) for i, b in enumerate(beatmap.beats))
+    beats_iter = ((i, b, int(b.time/dt)) for i, b in enumerate(beats))
     windowed_beats = ra.window(beats_iter, track_width, bar_offset, key=lambda a: a[2:]*2)
     next(windowed_beats)
 
-    hitted_indices = set()
-    scoring_index = 0
+    hitted_beats = set()
+    hit = None
+    hit_index = 0
+    beat_index = -1
 
-    current_time = yield None
     while True:
+        current_time = yield
+
         view = [" "]*track_width
         current_pixel = int(current_time/dt)
 
-        # updating hitted_indices, it also catches the last item in `scores`
-        for hit_time, hit_strength, hit_score, hit_index in scores[scoring_index:]:
-            if hit_index is not None and hit_score != HitScore.MISS:
-                hitted_indices.add(hit_index)
-        scoring_index = len(scores)
+        # updating `hitted_beats`, it also catches the last item in `hits`
+        for hit in hits[hit_index:]:
+            if not isinstance(hit, Hit.Unexpected):
+                beat_index += 1
+                if not isinstance(hit, Hit.Miss):
+                    hitted_beats.add(beat_index)
+            hit_index += 1
+
+        if hit is not None and abs(hit.time - current_time) >= sustain:
+            hit = None
 
         # draw un-hitted beats
         for index, beat, beat_pixel in windowed_beats.send(current_pixel):
-            if index not in hitted_indices:
+            if index not in hitted_beats:
                 # "░▄▀█", "□ ▣ ■"
-                if beat.type == BeatType.SOFT:
+                if isinstance(beat, Beat.Soft):
                     symbol = "+"
-                elif beat.type == BeatType.LOUD:
+                elif isinstance(beat, Beat.Loud):
                     symbol = "o"
                 else:
-                    symbol = "?"
+                    symbol = " "
                 view[beat_pixel - current_pixel + bar_offset] = symbol
 
         view[0] = "("
         view[bar_offset] = "|"
         view[-1] = ")"
 
-        if scoring_index > 0 and abs(hit_time - current_time) < sustain:
-            # visual feedback for hit
-            if hit_strength >= thresholds[1]:
+        if hit is not None:
+            # visual feedback for hit strength
+            if hit.strength >= thresholds[1]:
                 view[bar_offset] = "H"
-            elif hit_strength >= thresholds[0]:
+            elif hit.strength >= thresholds[0]:
                 view[bar_offset] = "I"
 
-            # visual feedback for score
+            # visual feedback for hit accuracy
             pass
 
-        current_time = yield "".join(view)
+        sys.stdout.write("".join(view) + "\r")
+        sys.stdout.flush()
 
 
 class Game:
@@ -180,7 +225,7 @@ class Game:
 
         return output_stream
 
-    def get_input_stream(self, beatmap, scores):
+    def get_input_stream(self, beatmap, hits):
         dect = ra.pipe(ra.frame(WIN_LENGTH, HOP_LENGTH),
                        ra.power_spectrum(RATE, WIN_LENGTH),
                        ra.onset_strength(RATE/WIN_LENGTH, HOP_LENGTH/RATE),
@@ -188,7 +233,7 @@ class Game:
                                        pre_max=PRE_MAX, post_max=POST_MAX,
                                        pre_avg=PRE_AVG, post_avg=POST_AVG,
                                        wait=WAIT, delta=DELTA),
-                       judge(beatmap, scores, self.thresholds, self.judgements))
+                       judge(beatmap.beats, hits, self.thresholds, self.judgements))
         next(dect)
 
         def input_callback(in_data, frame_count, time_info, status):
@@ -210,24 +255,20 @@ class Game:
 
         return input_stream
 
-    def get_view_callback(self, beatmap, scores):
-        drawer = draw_track(beatmap, scores, self.thresholds, self.sustain,
-                            self.track_width, self.bar_offset, self.drop_speed)
+    def get_view_callback(self, beatmap, hits):
+        drawer = draw_track(beatmap.beats, hits,
+                            self.track_width, self.bar_offset, self.drop_speed,
+                            self.thresholds, self.sustain)
         next(drawer)
 
-        def view_callback(current_time):
-            view = drawer.send(current_time)
-            sys.stdout.write(view + "\r")
-            sys.stdout.flush()
-
-        return view_callback
+        return drawer.send
 
     def play(self, beatmap):
-        scores = []
+        hits = []
 
         output_stream = self.get_output_stream(beatmap)
-        input_stream = self.get_input_stream(beatmap, scores)
-        view_callback = self.get_view_callback(beatmap, scores)
+        input_stream = self.get_input_stream(beatmap, hits)
+        view_callback = self.get_view_callback(beatmap, hits)
 
         reference_time = time.time()
         input_stream.start_stream()
@@ -242,20 +283,20 @@ class Game:
         output_stream.close()
         input_stream.close()
 
-        return scores
+        return hits
 
 
 # test
 beatmap = Beatmap(16.5,
-                  Soft(1.0),  Soft(2.0),  Soft(3.0),  Soft(3.5),  Loud(4.0),
-                  Soft(5.0),  Soft(6.0),  Soft(7.0),  Soft(7.5),  Loud(8.0),
-                  Soft(9.0),  Soft(10.0), Soft(11.0), Soft(11.5), Loud(12.0),
-                  Soft(13.0), Soft(14.0), Soft(15.0), Soft(15.5), Loud(16.0))
+                  Beat.Soft(1.0),  Beat.Soft(2.0),  Beat.Soft(3.0),  Beat.Soft(3.5),  Beat.Loud(4.0),
+                  Beat.Soft(5.0),  Beat.Soft(6.0),  Beat.Soft(7.0),  Beat.Soft(7.5),  Beat.Loud(8.0),
+                  Beat.Soft(9.0),  Beat.Soft(10.0), Beat.Soft(11.0), Beat.Soft(11.5), Beat.Loud(12.0),
+                  Beat.Soft(13.0), Beat.Soft(14.0), Beat.Soft(15.0), Beat.Soft(15.5), Beat.Loud(16.0))
 
 game = Game(thresholds=(0.1, 2.0), judgements=(0.14, 0.10, 0.06, 0.02),
             track_width=100, bar_offset=10, drop_speed=100.0, sustain=0.2)
 
-scores = game.play(beatmap)
+hits = game.play(beatmap)
 print()
-for score in scores:
-    print(score)
+for hit in hits:
+    print(hit)
