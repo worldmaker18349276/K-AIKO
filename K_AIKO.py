277c2f6835edd9fce17b50760383bb58c660c96a
worldmaker <worldmaker18349276@gmail.com>
2020-09-16 02:08:16 +0800

    add K_AIKO.py

---
+++ K_AIKO.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+from knock import *
+from beatmap import *
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    
+    with open(filename) as file:
+        sheet = BeatmapStdSheet()
+        exec(file.read(), dict(), dict(sheet=sheet))
+        beatmap = Beatmap(sheet.audio, sheet.events)
+
+        console = KnockConsole()
+        console.play(beatmap)
+
+        print()
+        for event in beatmap.events:
+            print(event)
