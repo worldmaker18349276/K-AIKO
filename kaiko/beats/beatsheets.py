ca6f6e4f0984baf10e30e3e95282e581eb2a66f0
worldmaker <worldmaker18349276@gmail.com>
2022-09-10 15:53:52 +0800

    add mstr/rmstr serializers for beatsheets

--- kaiko/beats/beatsheets.py
+++ kaiko/beats/beatsheets.py
@@ -44,29 +44,6 @@ def read(filename, hack=False, metadata_only=False):
         raise BeatmapParseError(f"unknown file extension: {filename}")
 
 
-mstr_parser = (
-    pc.regex(
-        # always start/end with newline
-        r'"""(?=\n)('
-        r'(?!""")[^\\\x00]'
-        r"|\\[0-7]{1,3}"
-        r"|\\x[0-9a-fA-F]{2}"
-        r"|\\u[0-9a-fA-F]{4}"
-        r"|\\U[0-9a-fA-F]{8}"
-        r"|\\(?![xuUN\x00])."
-        r')*(?<=\n)"""',
-    )
-    .map(ast.literal_eval)
-    .desc("triple quoted string")
-)
-
-rmstr_parser = (
-    pc.regex(r'r"""(?=\n)(' r'(?!""")[^\\\x00]' r"|\\[^\x00]" r')*(?<=\n)"""')
-    .map(ast.literal_eval)
-    .desc("raw triple quoted string")
-)
-
-
 def make_mstr_serializer(suggestions=[]):
     parser = (
         pc.regex(
@@ -90,13 +67,45 @@ def make_mstr_serializer(suggestions=[]):
         else:
             return set()
 
-    def format_mstr(value):
-        return '"""%s"""' % "\n".join(
-            repr(line + '"')[1:-2].replace('"""', r'\"""').replace(r"\'", "'")
-            for line in value.split("\n")
+    def formatter(value):
+        return (
+            '"""%s"""' %
+            repr(value + '"')[1:-2]
+            .replace('"', r"\"")
+            .replace(r"\'", "'")
+            .replace(r"\n", "\n")
         )
 
-    return sz.Serializer(parser, format_mstr, validator).suggest(suggestions or ["\n"])
+    return sz.Serializer(parser, formatter, validator).suggest(suggestions or ["\n"])
+
+
+def make_rmstr_serializer(suggestions=[]):
+    parser = (
+        pc.regex(
+            # always start/end with newline
+            r'r"""(?=\n)('
+            r'(?!""")[^\\\x00]'
+            r"|\\[^\x00]"
+            r')*(?<=\n)"""'
+        )
+        .map(ast.literal_eval)
+        .desc("raw triple quoted string")
+    )
+
+    def validator(value):
+        if isinstance(value, str) and value.startswith("\n") and value.endswith("\n"):
+            return {str}
+        else:
+            return set()
+
+    def formatter(value):
+        if re.search(r'\x00|\r|"""|\\$', value):
+            raise ValueError(
+                "string cannot contain '\\x00', '\\r', '\"\"\"' and single '\\'"
+            )
+        return 'r"""' + value + '"""'
+
+    return sz.Serializer(parser, formatter, validator).suggest(suggestions or ["\n"])
 
 
 @pc.parsec
@@ -135,14 +144,6 @@ def make_msp_parser(indent=0):
     return ""
 
 
-_beatmap_fields = {
-    "info": str,
-    "audio.path": str,
-    "audio.volume": float,
-    "audio.preview": float,
-}
-
-
 @pc.parsec
 def make_beatmap_parser(filepath, metadata_only=False):
     beatmap_name = "beatmap"
@@ -171,11 +172,22 @@ def make_beatmap_parser(filepath, metadata_only=False):
 
     beatmap = Beatmap(filepath)
 
+    # parsers
+    mstr_parser = make_mstr_serializer().parser
+    rmstr_parser = make_rmstr_serializer().parser
+    str_parser = sz.make_str_serializer().parser
+    float_parser = sz.make_float_serializer().parser
+
     # parse fields
-    valid_fields = dict(_beatmap_fields)
-    valid_fields["tracks"] = dict
-    valid_fields["beatstate"] = object
-    valid_fields["beatpoints"] = object
+    valid_fields = [
+        "info",
+        "audio.path",
+        "audio.volume",
+        "audio.preview",
+        "beatstate",
+        "beatpoints",
+        "tracks",
+    ]
 
     while True:
         sp = yield make_msp_parser(indent=0).reject(
@@ -185,23 +197,52 @@ def make_beatmap_parser(filepath, metadata_only=False):
             return beatmap
 
         yield pc.string(f"{beatmap_name}.")
-        name = yield pc.tokens(list(valid_fields.keys()))
+        name = yield pc.tokens(valid_fields)
 
-        if name == "tracks":
-            # parse track:
+        if name == "info":
+            yield pc.string(" = ")
+            beatmap.info = yield mstr_parser
+            valid_fields.remove(name)
 
-            # beatmap.tracks["main"] = BeatTrack.parse(r"""
+        elif name == "audio.path":
+            yield pc.string(" = ")
+            beatmap.audio.path = yield str_parser
+            valid_fields.remove(name)
+
+        elif name == "audio.volume":
+            yield pc.string(" = ")
+            beatmap.audio.volume = yield float_parser
+            valid_fields.remove(name)
+
+        elif name == "audio.preview":
+            yield pc.string(" = ")
+            beatmap.audio.preview = yield float_parser
+            valid_fields.remove(name)
+
+        elif name == "beatpoints":
+            # parse beatpoints:
+
+            # beatmap.beatpoints = BeatPoints.parse(r"""
             # ...
             # """)
 
-            track_name, = yield pc.template("[{}]", sz.make_str_serializer().parser)
-            if track_name in beatmap.tracks:
-                raise ValueError("duplicated name")
             yield pc.string(" = ")
-            track_str, = yield pc.template("BeatTrack.parse({})", rmstr_parser)
 
-            track = BeatTrack.parse(track_str) if not metadata_only else BeatTrack([])
-            beatmap.tracks[track_name] = track
+            try:
+                offset, tempo = yield pc.template("BeatPoints.fixed(offset={}, tempo={})", float_parser, float_parser)
+            except pc.ParseFailure:
+                pass
+            else:
+                beatpoints = BeatPoints.fixed(offset=offset, tempo=tempo)
+                beatmap.beatpoints = beatpoints
+                continue
+
+            beatpoints_str, = yield pc.template("BeatPoints.parse({})", rmstr_parser)
+
+            beatpoints = BeatPoints.parse(beatpoints_str) if not metadata_only else BeatPoints([])
+            beatmap.beatpoints = beatpoints
+
+            valid_fields.remove(name)
 
         elif name == "beatstate":
             # parse beatstate:
@@ -228,68 +269,26 @@ def make_beatmap_parser(filepath, metadata_only=False):
             beatstate = BeatState.parse(beatbarstate_str) if not metadata_only else BeatState([])
             beatmap.beatstate = beatstate
 
-        elif name == "beatpoints":
-            # parse beatpoints:
+            valid_fields.remove(name)
 
-            # beatmap.beatpoints = BeatPoints.parse(r"""
+        elif name == "tracks":
+            # parse track:
+
+            # beatmap.tracks["main"] = BeatTrack.parse(r"""
             # ...
             # """)
 
+            track_name, = yield pc.template("[{}]", sz.make_str_serializer().parser)
+            if track_name in beatmap.tracks:
+                raise ValueError("duplicated name")
             yield pc.string(" = ")
+            track_str, = yield pc.template("BeatTrack.parse({})", rmstr_parser)
 
-            float_parser = sz.make_float_serializer().parser
-            try:
-                offset, tempo = yield pc.template("BeatPoints.fixed(offset={}, tempo={})", float_parser, float_parser)
-            except pc.ParseFailure:
-                pass
-            else:
-                beatpoints = BeatPoints.fixed(offset=offset, tempo=tempo)
-                beatmap.beatpoints = beatpoints
-                continue
-
-            beatpoints_str, = yield pc.template("BeatPoints.parse({})", rmstr_parser)
-
-            beatpoints = BeatPoints.parse(beatpoints_str) if not metadata_only else BeatPoints([])
-            beatmap.beatpoints = beatpoints
+            track = BeatTrack.parse(track_str) if not metadata_only else BeatTrack([])
+            beatmap.tracks[track_name] = track
 
         else:
-            yield pc.string(" = ")
-            field_type = valid_fields[name]
-            value = (
-                yield mstr_parser
-                if name == "info"
-                else sz.make_serializer_from_type_hint(field_type).parser
-            )
-            del valid_fields[name]
-
-            subfield = beatmap
-            for field in name.split(".")[:-1]:
-                subfield = getattr(subfield, field)
-            setattr(subfield, name.split(".")[-1], value)
-
-
-def format_mstr(value):
-    if not value.startswith("\n") or not value.endswith("\n"):
-        raise ValueError("string should start and end with newline")
-    return (
-        '"""'
-        + repr(value + '"')[1:-2]
-        .replace('"', r"\"")
-        .replace(r"\'", "'")
-        .replace(r"\n", "\n")
-        + '"""'
-    )
-
-
-def format_rmstr(value):
-    if not value.startswith("\n") or not value.endswith("\n"):
-        raise ValueError("string should start and end with newline")
-    m = re.search(r'\x00|\r|"""|\\$', value)
-    if m:
-        raise ValueError(
-            "string cannot contain '\\x00', '\\r', '\"\"\"' and single '\\'"
-        )
-    return 'r"""' + value + '"""'
+            assert False
 
 
 def format_beatmap(beatmap):
@@ -301,15 +300,24 @@ def format_beatmap(beatmap):
     res.append("\n")
     res.append(f"{beatmap_name} = Beatmap(__file__)\n")
 
-    for name, typ in _beatmap_fields.items():
-        format_field = (
-            format_mstr
-            if name == "info"
-            else sz.make_serializer_from_type_hint(typ).formatter
-        )
-        res.append(f"{beatmap_name}.{name} = {format_field(getattr(beatmap, name))}\n")
+    format_mstr = make_mstr_serializer().formatter
+    format_rmstr = make_rmstr_serializer().formatter
+    format_str = sz.make_str_serializer().formatter
+    format_float = sz.make_float_serializer().formatter
 
-    format_str = sz.make_serializer_from_type_hint(str).formatter
+    res.append(f"{beatmap_name}.info = {format_mstr(beatmap.info)}\n")
+    res.append(f"{beatmap_name}.audio.path = {format_str(beatmap.audio.path)}\n")
+    res.append(f"{beatmap_name}.audio.volume = {float_float(beatmap.audio.volume)}\n")
+    res.append(f"{beatmap_name}.audio.preview = {float_float(beatmap.audio.preview)}\n")
+
+    res.append(
+        f"{beatmap_name}.beatpoints = BeatPoints.parse({format_rmstr(...)})\n"
+    )
+
+    res.append(
+        f"{beatmap_name}.beatstate = BeatState.parse({format_rmstr(...)})\n"
+    )
+
     for track_name, track in beatmap.tracks.items():
         track_str = "\n" + track.to_str() + "\n"
         res.append(
