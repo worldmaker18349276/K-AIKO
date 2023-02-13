079aff616e56e6fde6125e6a2c80779b0b9be6ab
worldmaker <worldmaker18349276@gmail.com>
2023-02-13 22:10:56 +0800

    refine providers

--- kaiko/utils/providers.py
+++ kaiko/utils/providers.py
@@ -1,9 +1,15 @@
 import contextlib
 import functools
+from dataclasses import dataclass
 
 
 class ServiceError(Exception):
     pass
+
+
+@dataclass
+class ServiceItem:
+    service: object
 
 
 _services = []
@@ -11,37 +17,37 @@ _services = []
 
 def get(typ):
     global _services
-    for service in reversed(_services):
-        if typ == type(service):
-            return service
+    for item in reversed(_services):
+        if typ == type(item.service):
+            return item.service
     else:
         raise ServiceError(f"Service not found: {typ}")
 
 
 def set_static(service):
     global _services
-    _services.insert(0, service)
+    _services.append(ServiceItem(service))
 
 
 @contextlib.contextmanager
 def set(*services):
     global _services
-    length = len(services)
-    _services.extend(services)
+    items = [ServiceItem(service) for service in services]
+    _services.extend(items)
     try:
         yield
     finally:
-        if length > 0:
-            del _services[-length:]
+        for item in items:
+            _services.remove(item)
 
 
 def inject(**services):
     def inject_decorator(func):
         @functools.wraps(func)
         def injected_func(*args, **kw):
-            for name, service in services.items():
+            for name, typ in services.items():
                 if name not in kw:
-                    kw[name] = get(service)
+                    kw[name] = get(typ)
             return func(*args, **kw)
 
         return injected_func
